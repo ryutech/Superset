fcb2586c4c031dddd3c0ab9cba3adc0cf1f589b1
Conglei Shi <conglei.shi@airbnb.com>
2018-12-12 17:47:02 -0800

    fix flake8

--- superset/views/api.py
+++ superset/views/api.py
@@ -21,13 +21,13 @@ from flask import request
 from flask_appbuilder import expose
 from flask_appbuilder.security.decorators import has_access_api
 
-import superset.models.core as models
-
 from superset import appbuilder, db, security_manager
 from superset.common.query_context import QueryContext
 from superset.legacy import update_time_range
+import superset.models.core as models
 from superset.models.core import Log
 from .base import api, BaseSupersetView, handle_api_exception
+
 
 class Api(BaseSupersetView):
     @Log.log_this
@@ -66,7 +66,6 @@ class Api(BaseSupersetView):
         update_time_range(form_data)
 
         return json.dumps(form_data)
-
 
 
 appbuilder.add_view_no_menu(Api)
