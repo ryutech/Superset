79d912e703e02f2687e687433a13996147d58ad2
Beto Dealmeida <roberto@dealmeida.net>
2020-07-01 17:49:43 -0700

    Fix read only tags

--- superset-frontend/src/components/ObjectTags.jsx
+++ superset-frontend/src/components/ObjectTags.jsx
@@ -124,7 +124,7 @@ export default class ObjectTags extends React.Component {
         {this.state.tags.map(tag => (
           <Label bsStyle="info">
             <a
-              href={`/superset/welcome?filters=%5B%5D&pageIndex=0&search=${this.props.tag.name}&sortColumn=changed_on&sortOrder=desc`}
+              href={`/superset/welcome?filters=%5B%5D&pageIndex=0&search=${tag.name}&sortColumn=changed_on&sortOrder=desc`}
               className="deco-none"
             >
               {tag.name}
